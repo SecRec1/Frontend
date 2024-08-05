4dd7e60a3eaf070ecea4c903c60fa6dee93cb252
SecRec1 <Chris@securerecycling.com>
2024-08-05 09:37:13 -0400

    server.js changes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -20,6 +22,13 @@ app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server started on http://0.0.0.0:${port}`);
+// Read SSL certificate and key
+const sslOptions = {
+  key: fs.readFileSync('example-key.pem'),
+  cert: fs.readFileSync('example.pem')
+};
+
+// Create HTTPS server
+https.createServer(sslOptions, app).listen(port, '0.0.0.0', () => {
+  console.log(`HTTPS Server started on https://0.0.0.0:${port}`);
 });
